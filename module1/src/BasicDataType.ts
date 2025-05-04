854864f64c998336c036ba2cee67bcdbd5eb9a41
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-04 20:29:56 +0600

    data

---
+++ module1/src/BasicDataType.ts
@@ -0,0 +1,29 @@
+// ------- Basic Data Type use in TypeScript
+
+// Primitive Data type-------
+
+// ------- String
+
+let firstname:string = 'Madina'
+
+// ------ Number
+
+let roll:number = 20
+
+// ------ Boolean
+
+let isAdmin:boolean = true
+
+// ------- Null
+
+let NameField:null = null
+
+// ------ Undefined
+
+let Myname:undefined = undefined
+
+
+// Non Primitive Data Type 
+
+// -------- Array 
+
