ccba3f047ab869902c3bebae0d269ddcb3fd20ea
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-05 00:22:50 +0600

    methood

---
+++ module1/src/function1.6.ts
@@ -0,0 +1,23 @@
+// Function in Ts 
+
+// ----- Normal Function 
+
+function add (num1:number, num2:number):number{
+
+    return num1+num2
+}
+
+// ----- Arrow Function
+
+const addArrow = (num1:number, num2:number): number=>num1+num2
+
+// Object --> function --> method
+
+const poorUser = {
+
+    name: 'Madina',
+    balance: 0,
+    addBalance (balance : number): string {
+        return `My new balance is : ${this.balance + balance}`
+    }
+}
