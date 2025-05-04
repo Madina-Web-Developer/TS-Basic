f271ace92155c2ef4ac2f10cb78e9bd2cf3d3abf
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-04 22:02:58 +0600

    object

--- dist/module1/src/BasicDataType.js
+++ dist/module1/src/BasicDataType.js
@@ -1,1 +1,24 @@
 "use strict";
+// ------- Basic Data Type use in TypeScript
+// Primitive Data type-------
+// ------- String
+let firstname = 'Madina';
+// ------ Number
+let roll = 20;
+// ------ Boolean
+let isAdmin = true;
+// ------- Null
+let NameField = null;
+// ------ Undefined
+let Myname = undefined;
+// Non Primitive Data Type 
+// -------- Array 
+let Friends = ['madina', 'Mina', 'tara'];
+Friends.push('madina');
+let RollnumberList = [1, 2, 3];
+RollnumberList.push(2);
+// -------- Tuple 
+// tuple --- array---order---type of values
+let coordinator = [1, 2];
+let mixedup = ['madina', 22];
+let ageName = [22, 'madina', true];
---
+++ module1/src/refTypeObject1.5.ts
@@ -0,0 +1,17 @@
+//  reference Type ---> Object
+
+
+const user:{
+
+    Company:'Programing hero';  // literal type
+    firstName:string;
+    MiddleName?:string; // ? ayta daor mane hosse optional jodi thake tahola deba but nadelao Error deba na
+    LastName:string;
+    Maritalstatus?:boolean;
+}={
+    Company:'Programing hero',  // literal type
+    firstName:'Madina',
+    LastName:'Mina',
+    Maritalstatus:true,
+}
+
