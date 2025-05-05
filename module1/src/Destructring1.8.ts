fc1497f253c412a4594dc099720c0538321c3803
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-05 13:17:44 +0600

    hhhs

--- module1/src/Destructring1.8.ts
+++ module1/src/Destructring1.8.ts
@@ -25,3 +25,9 @@ const {contno} = user;
 const {name:firstname} =user;
 
 }
+
+// ------------------ Array Destructring
+
+const myFds = ['asa', 'nasa', 'baul', 'gaul', 'faul'];
+
+const [,, a,b,...rest]=myFds;   "aykhana prothom 2 ta name neta na chayla arokom koma ,, deta hobe and baki gula last porjonto nita chayla ...rest dela hoye jabe"
