e6300f5b3511f143097d8c150d8e9d06b928e8b8
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-05 16:44:56 +0600

    intersection complite

--- dist/module1/src/TypeAlian1.9.js
+++ dist/module1/src/TypeAlian1.9.js
@@ -1,1 +1,27 @@
 "use strict";
+{
+    const student1 = {
+        Name: 'madina',
+        age: 22,
+        contactno: '0171111111',
+        address: 'dhk',
+        gender: 'female',
+        status: true,
+    };
+    const student2 = {
+        Name: 'mina',
+        age: 28,
+        address: 'dhk',
+        gender: 'female',
+        status: true,
+    };
+    const student3 = {
+        Name: 'Tina',
+        age: 25,
+        address: 'dhk',
+        gender: 'female',
+    };
+    const name = 'parsian';
+    const isadmin = true;
+    const sum = (num1, num2) => num1 + num2;
+}
---
+++ module1/src/Union & Intersection 1.10.ts
@@ -0,0 +1,59 @@
+// --------Union & Intersection in TypeScript 
+
+{
+
+// ----------- Union Types 
+
+type FrontendDev = 'good'| 'bad';
+
+type BackendDev = 'expert' | 'normal'
+
+type FullstackDev = FrontendDev | BackendDev
+
+// another way 
+
+
+type User ={
+
+    name:string;
+    age:number;
+    email?:string;
+    gender:'male'|'female'
+    bloodgroup:'A+'| 'B+' | 'AB+'
+}
+
+const user1:User ={
+
+    name:'madina',
+    age:22,
+    gender:'female',
+    bloodgroup:'A+'
+}
+
+
+// --------------------------- Intersection for TypeScript
+
+
+type FrontendDeveloper = {
+
+    Skills :string[],
+    Deg1:'FD',
+}
+type BackendDeveloper = {
+
+    Skills :string[],
+    Deg2:'BD',
+}
+
+type Developer = FrontendDeveloper & BackendDeveloper
+
+const FullstackDeveloper : Developer = {
+
+    Skills :['HTML', 'Express', 'CSS'],
+    Deg1:'FD',
+    Deg2:'BD',
+}
+
+
+
+}
