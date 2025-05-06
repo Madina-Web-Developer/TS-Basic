e0d61813d6dc7e13077f9b748170b067c6b8bc7f
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-06 12:05:45 +0600

    t

--- dist/Module2/2.1.js
+++ dist/Module2/2.1.js
@@ -1,1 +1,19 @@
 "use strict";
+{
+    // -------- Type Assertion------------->
+    let anything;
+    anything = 'This is a massage';
+    anything = 222;
+    anything;
+    const KgtoGrm = (value) => {
+        if (typeof value === 'string') {
+            const convertedvalue = parseFloat(value) * 1000;
+            return `The converted value is ${convertedvalue}`;
+        }
+        if (typeof value === 'number') {
+            return value * 1000;
+        }
+    };
+    const result1 = KgtoGrm(1000);
+    const result2 = KgtoGrm("1000");
+}
