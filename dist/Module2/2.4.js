89f4d41654de1dfdddc5df5d45c2264b33185313
Madina-Web-Developer <developermadina761@gmail.com>
2025-05-06 23:57:05 +0600

    function

--- dist/Module2/2.4.js
+++ dist/Module2/2.4.js
@@ -1,1 +1,30 @@
 "use strict";
+// ----------- Interface -----> Generic
+{
+    ;
+    const RichDeveloper = {
+        name: 'mina',
+        computer: {
+            Brand: 'HP',
+            Model: 'mp5'
+        },
+        Mobile: {
+            Brand: 'realme'
+        },
+        Bike: {
+            Brand: 'Yahama',
+            Model: 'mmm9'
+        },
+    };
+    const PoorDeveloper = {
+        name: 'mina',
+        computer: {
+            Brand: 'HP',
+            Model: 'mp5'
+        },
+        Mobile: {
+            Brand: 'realme',
+            Model: 'C75'
+        },
+    };
+}
